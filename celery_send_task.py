3c61784215c4a6709a9d92fd9e12d802df3c31f2
ttcool <ttcool@live.cn>
2021-05-08 10:41:51 +0800

    Update celery_send_task.py

--- celery_send_task.py
+++ celery_send_task.py
@@ -7,4 +7,5 @@ from celery import Celery
 
 app = Celery('celeryapp',broker='redis://192.168.5.108:6379/2',backend='redis://192.168.5.108:6379/3')
 result = app.send_task('celeryapp.task1.hello',['hello','world'],queue='default')
+#queue指定任务发送到指定的队列，不指定默认使用celery默认队列
 print(result.successful())
